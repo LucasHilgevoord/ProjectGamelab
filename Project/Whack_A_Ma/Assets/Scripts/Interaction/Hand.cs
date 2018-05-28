c4c57e5ca9be7383275aebd295662b6520da6b02
wakamantroy <troy_toendang@hotmail.com>
2018-05-28 13:26:17 +0200

    Sprint 01 merge

---
+++ Project/Whack_A_Ma/Assets/Scripts/Interaction/Hand.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    RaycastCheck raycastCheck;
+    RaycastHit2D hit;
+
+    private Animator anim;
+    private AudioSource aS;
+
+    public Transform mHandMesh;
+
+    void Start()
+    {
+        raycastCheck = GetComponent<RaycastCheck>();
+        anim = GetComponent<Animator>();
+        aS = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        mHandMesh.position = Vector3.Lerp(mHandMesh.position, transform.position, Time.deltaTime * 15.0f);
+        Shoot();
+    }
+
+    void Shoot()
+    {
+        hit = raycastCheck.hit;
+
+        if (raycastCheck.RaycastChecker() == "ShootAble")
+        {
+            Debug.Log("SHOT!!");
+            StartCoroutine(OnShoot());
+        }
+    }
+
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint) + offset;
+        transform.position = cursorPosition;
+    }
+
+    IEnumerator OnShoot()
+    {
+        float timer = 0f;
+        float timeMultiplier = 1f;
+        float duration = .5f;
+
+        while (raycastCheck.RaycastChecker() == "ShootAble")
+        {
+            if (timer <= duration)
+            {
+                //Debug.Log(timer);
+                anim.SetBool("Indicator", true);
+                timer += timeMultiplier * Time.deltaTime;
+            }
+            else
+            {
+                aS.Play();
+                anim.SetBool("Indicator", false);
+                Destroy(hit.collider.gameObject);
+            }
+            yield return null;
+        }
+
+        if (raycastCheck.RaycastChecker() != "ShootAble")
+        {
+            anim.SetBool("Indicator", false);
+        }
+    }
+}
---
+++ Project/Whack_A_Ma/Assets/Scripts/Other/StrengthIndicator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrengthIndicator : MonoBehaviour
+{
+    private Animator anim;
+
+    // 1. On hold, let a value count up.
+    // 2. Indicate the bar to fill up.
+    // 3. On release, reset.
+
+    private float countIndicator = 0f;
+    private float countingSpeed = 1f;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        Debug.Log(countIndicator);
+        Timer();
+	}
+
+    void Timer()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            countIndicator += countingSpeed * Time.deltaTime;
+            anim.SetBool("Indicator", true);
+        }
+        else
+        {
+            countIndicator = 0f;
+            anim.SetBool("Indicator", false);
+        }
+    }
+}
